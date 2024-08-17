8578bf7430ffdfa4abda16e7db11c8fd8385496e
Anton <ANTON-1996@yandex.ru>
2024-08-17 18:06:50 +0300

    исправление перебора csv

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import csv
 
 
-def indent(elem, level=0):
+def indent(elem: any, level: int = 0) -> None:
     '''создает красивый xml c отступами'''
     i = "\n" + level*"  "
     if len(elem):
@@ -19,19 +19,25 @@ def indent(elem, level=0):
             elem.tail = i
 
 
-def app_cis():
-    flag_first_chek = True
-    with open('1.csv', encoding='utf-8') as f:
-        lines = csv.reader(f)
-        for line in lines:
-            new_item = ET.Element('cis')
-            if flag_first_chek:
-                new_item.text = line[0].strip()[1:]
-                flag_first_chek = False
-            else:
-                new_item.text = line[0].strip()
-            root[0][1].insert(1, new_item)
-    return None
+def csv_dict() -> dict:
+    result = {}
+    with open('input.csv', encoding='utf-8') as f:
+        csv_file = csv.DictReader(f, delimiter=',')
+        for item in csv_file:
+            result.setdefault(item['set'], []).append(item['code'])
+    return result
+
+
+def app_cis(elem: any) -> None:
+    pack_content_item
+    for k, v in csv_dict().items():
+        new_item = ET.Element('cis')
+        if flag_first_chek:
+            new_item.text = line[0].strip()[1:]
+            flag_first_chek = False
+        else:
+            new_item.text = line[0].strip()
+        elem[0][1].insert(1, new_item)
 
 
 if __name__ == '__main__':
